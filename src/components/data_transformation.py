5c5a5a511aff288adfe17716af19a5fe40389eea
ash81166 <aishikchatterjee12@gmail.com>
2023-09-05 23:29:23 +0530

    preprocessing done

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -6,12 +6,12 @@ import pandas as pd
 import os 
 
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import ColumnTransformer
-from sklearn.preprocessing import StandardScaler 
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import StandardScaler,OrdinalEncoder
 
 from src.exception import CustomException
 from src.logger import logging 
+from src.utils import save_object
 
 @dataclass 
 class DataTransformationConfig:
@@ -19,24 +19,13 @@ class DataTransformationConfig:
 
 class DataTransformation:
     def __init__(self):
-        self.data_transformation_config=DataTransformationConfig
+        self.data_transformation_config=DataTransformationConfig()
     
     def get_data_transformer_object(self):
         try:
-            features=['class','cap-shape','cap-surface','cap-color','bruises','odor','gill-attachment','gill-spacing','gill-size','gill-color',
-                      'stalk-shape','stalk-root','stalk-surface-above-ring','stalk-surface-below-ring','stalk-color-above-ring',
-                      'stalk-color-below-ring','veil-color','ring-number','ring-type','spore-print-color','population,habitat']
-            pipeline=Pipeline(
-                steps=[
-                    ('label_encoder',LabelEncoder()),
-                    ('Standard scaling', StandardScaler())
-                ]
-            )
-            logging.info("Label encoding and scaling completed.")
-
-            preprocessor=ColumnTransformer(
-                
-            )
+            columns = ['class','odor','gill-size','gill-color','stalk-shape','stalk-root','spore-print-color','population']
+            pipeline = Pipeline([('last_hope', OrdinalEncoder()),('standard_scaler', StandardScaler())])
+            preprocessor=ColumnTransformer([('pipe',pipeline,columns)])
             return preprocessor
             
             
@@ -46,12 +35,24 @@ class DataTransformation:
     def initiate_data_transformation(self,train_path,test_path):
         try:
             train_df=pd.read_csv(train_path)
-            test_path=pd.read_cav(test_path)
+            test_df=pd.read_csv(test_path)
             logging.info("read train and test data completed")
             
             logging.info("obtaining perprocessing obj")
-            preprocessor_obj=self.get_data_transformer_object()
-            target_column_name="class"
+            preprocessing_obj=self.get_data_transformer_object()
+            
+            train_arr=preprocessing_obj.fit_transform(train_df)
+            test_arr=preprocessing_obj.fit_transform(test_df)
+            logging.info('Transformation of train and test done')            
+            
+            logging.info("Save Preprocessing Object")
+            save_object(
+                file_path=self.data_transformation_config.preprocessor_obj_file_path,
+                obj=preprocessing_obj
+            )
+            return (
+                train_arr, test_arr,self.data_transformation_config.preprocessor_obj_file_path,
+            )
             
         except Exception as e:
             raise CustomException(e,sys)
